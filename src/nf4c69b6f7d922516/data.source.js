d3d4ed004270a17407465c0d54b6fbc5e7baeb92
Jade Allen Cook <jadeallencook@gmail.com>
2018-12-12 20:11:54 -0500

    🛠 Updated anxiety JSON and functionality

--- src/nf4c69b6f7d922516/data.source.js
+++ src/nf4c69b6f7d922516/data.source.js
@@ -1,56 +1,57 @@
 module.exports = [{
     title: 'Steps for Parents',
     subheader: 'Cras mattis consectetur purus sit amet fermentum?',
-    html: 'Maecenas faucibus mollis interdum. Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Curabitur blandit tempus porttitor.<br /><br />Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui.'
+    html: `Maecenas faucibus mollis interdum. Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Curabitur blandit tempus porttitor.<br /><br />Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui.`
 }, {
     title: 'Steps for Teens',
     subheader: 'Donec ullamcorper nulla non metus auctor fringilla?',
     icons: [{
         image: 'assets/images/mental-health-worry.svg',
         title: 'Worry Time',
-        html: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+        html: `Set a limited but specific time to worry. When something starts bugging you during the day, acknowledge it and tell it you'll look at it during worry time. Then put it in an imaginary box that you'll open then. Be specific: 10 p.m.`
     }, {
         image: 'assets/images/mental-health-unplug.svg',
         title: 'Unplug',
-        html: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+        html: `While not all anxiety stems from or is related to social media, anyone who's anxious may find that a break from the curated world of Instagram and the constant offerings of YouTube can be refreshing.`
     }, {
         image: 'assets/images/mental-health-heart.svg',
         title: 'Touch Your Heart',
-        html: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+        html: `Listen to your heartbeat for a few seconds to calm down or lower stress.`
     }, {
         image: 'assets/images/mental-health-talk.svg',
-        title: 'Talk To Someone',
-        html: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+        title: 'Talk It Out',
+        html: `Anxiety and depression often trap people — especially teens — in their own heads. Talking candidly with a trusted friend or family member can help lessen the feeling that "no one understands."`
     }, {
         image: 'assets/images/mental-health-head.svg',
-        title: 'Name The Emotion',
-        html: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+        title: 'Story In Your Head',
+        html: `When you speak your worries aloud, it's easier to see your panicked scenario as unlikely. Consider a boy waiting for a girl to respond to his prom invitation. His brain churns: She's giggling right now with her friends, who are going to ostracize him. Oh no. He'll have no friends. He'll never get married. "It carries emotion far beyond reality, but if you can speak it to yourself or a trusted friend, you quickly realize that's not the way it's going to be," says Joe Newman of Life Launch Centers.`
     }, {
         image: 'assets/images/mental-health-sleep.svg',
         title: 'Go To Sleep',
-        html: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+        html: `Everything is harder to deal with when you're tired. Nearly everyone needs more sleep than they get, especially teens. Get a better night's sleep by removing technological distractions.`
     }, {
-        image: 'assets/images/mental-health-calm.svg',
-        html: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+        image: 'assets/images/mental-health-eat-well.svg',
+        title: 'Eat Well',
+        html: `Relying on sugar, caffeine, refined carbs and empty calories can leave you lightheaded and prone to crashing, which can make anxiety worse. Eating good food in a mindful way can also be a chance to slow down and recalibrate.`
     }, {
         image: 'assets/images/mental-health-mindful.svg',
         title: 'Be Mindful',
-        html: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+        html: `Pay attention to your breath and scan your body with "loving kindness" — a good way to recognize "unpleasant emotions, aside from avoiding them," which "never reduces the stress," says Alexis V. Arczynski of the University of Utah Campus Counseling Center. Instead, acknowledge the feeling, either "swipe" it away or unpack it — no judgment for feeling how you feel.`
     }, {
         image: 'assets/images/mental-health-emotion.svg',
         title: 'Name The Emotion',
-        html: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+        html: `Identifying a stressful emotion can decrease its intensity by nearly half. Ask a child having an outburst to name what he's feeling — "Anger" "Sadness" "Fear" — and assign an intensity number, 1-10. It not only short-circuits the emotion, but forces the brain to think logically, shifting brain activity to the executive function, which imaging suggests goes dark when the amygdala is lit by emotion and anxiety.`
     }, {
         image: 'assets/images/mental-health-fear.svg',
-        title: 'Invite Fear',
-        html: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+        title: 'Welcome Fear',
+        html: `Make your home a place where discomfort and fear are welcome, says Utah therapist Jenny Howe. "Oh, this is something you're scared of?" she says, "Great, let's talk about it and do it."`
     }, {
         image: 'assets/images/mental-health-exercise.svg',
         title: 'Emphasize Exercise',
-        html: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+        html: `Physical activity releases stress while building confidence. It's also a good chance to make friends or learn new skills.`
     }, {
         image: 'assets/images/mental-health-breathing.svg',
         title: 'Box Breathing',
-        html: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+        html: `Do these steps for four seconds apiece. Breathe in. Hold. Breathe out. Hold. Pay attention as you do it and feel yourself calm.`
     }]
 }]
