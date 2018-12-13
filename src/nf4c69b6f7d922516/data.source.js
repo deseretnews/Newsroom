280724c8cd7543920993192fa6336b7fa2b32bea
Jade Allen Cook <jadeallencook@gmail.com>
2018-12-12 20:51:40 -0500

    ✏️ Decapititalized titles

--- src/nf4c69b6f7d922516/data.source.js
+++ src/nf4c69b6f7d922516/data.source.js
@@ -7,7 +7,7 @@ module.exports = [{
     subheader: 'Donec ullamcorper nulla non metus auctor fringilla?',
     icons: [{
         image: 'assets/images/mental-health-worry.svg',
-        title: 'Worry Time',
+        title: 'Worry time',
         html: `Set a limited but specific time to worry. When something starts bugging you during the day, acknowledge it and tell it you'll look at it during worry time. Then put it in an imaginary box that you'll open then. Be specific: 10 p.m.`
     }, {
         image: 'assets/images/mental-health-unplug.svg',
@@ -15,43 +15,43 @@ module.exports = [{
         html: `While not all anxiety stems from or is related to social media, anyone who's anxious may find that a break from the curated world of Instagram and the constant offerings of YouTube can be refreshing.`
     }, {
         image: 'assets/images/mental-health-heart.svg',
-        title: 'Touch Your Heart',
+        title: 'Touch your heart',
         html: `Listen to your heartbeat for a few seconds to calm down or lower stress.`
     }, {
         image: 'assets/images/mental-health-talk.svg',
-        title: 'Talk It Out',
+        title: 'Talk it out',
         html: `Anxiety and depression often trap people — especially teens — in their own heads. Talking candidly with a trusted friend or family member can help lessen the feeling that "no one understands."`
     }, {
         image: 'assets/images/mental-health-head.svg',
-        title: 'Story In Your Head',
+        title: 'Story in your head',
         html: `When you speak your worries aloud, it's easier to see your panicked scenario as unlikely. Consider a boy waiting for a girl to respond to his prom invitation. His brain churns: She's giggling right now with her friends, who are going to ostracize him. Oh no. He'll have no friends. He'll never get married. "It carries emotion far beyond reality, but if you can speak it to yourself or a trusted friend, you quickly realize that's not the way it's going to be," says Joe Newman of Life Launch Centers.`
     }, {
         image: 'assets/images/mental-health-sleep.svg',
-        title: 'Go To Sleep',
+        title: 'Go to sleep',
         html: `Everything is harder to deal with when you're tired. Nearly everyone needs more sleep than they get, especially teens. Get a better night's sleep by removing technological distractions.`
     }, {
         image: 'assets/images/mental-health-eat-well.svg',
-        title: 'Eat Well',
+        title: 'Eat well',
         html: `Relying on sugar, caffeine, refined carbs and empty calories can leave you lightheaded and prone to crashing, which can make anxiety worse. Eating good food in a mindful way can also be a chance to slow down and recalibrate.`
     }, {
         image: 'assets/images/mental-health-mindful.svg',
-        title: 'Be Mindful',
+        title: 'Be mindful',
         html: `Pay attention to your breath and scan your body with "loving kindness" — a good way to recognize "unpleasant emotions, aside from avoiding them," which "never reduces the stress," says Alexis V. Arczynski of the University of Utah Campus Counseling Center. Instead, acknowledge the feeling, either "swipe" it away or unpack it — no judgment for feeling how you feel.`
     }, {
         image: 'assets/images/mental-health-emotion.svg',
-        title: 'Name The Emotion',
+        title: 'Name the emotion',
         html: `Identifying a stressful emotion can decrease its intensity by nearly half. Ask a child having an outburst to name what he's feeling — "Anger" "Sadness" "Fear" — and assign an intensity number, 1-10. It not only short-circuits the emotion, but forces the brain to think logically, shifting brain activity to the executive function, which imaging suggests goes dark when the amygdala is lit by emotion and anxiety.`
     }, {
         image: 'assets/images/mental-health-fear.svg',
-        title: 'Welcome Fear',
+        title: 'Welcome fear',
         html: `Make your home a place where discomfort and fear are welcome, says Utah therapist Jenny Howe. "Oh, this is something you're scared of?" she says, "Great, let's talk about it and do it."`
     }, {
         image: 'assets/images/mental-health-exercise.svg',
-        title: 'Emphasize Exercise',
+        title: 'Emphasize exercise',
         html: `Physical activity releases stress while building confidence. It's also a good chance to make friends or learn new skills.`
     }, {
         image: 'assets/images/mental-health-breathing.svg',
-        title: 'Box Breathing',
+        title: 'Box breathing',
         html: `Do these steps for four seconds apiece. Breathe in. Hold. Breathe out. Hold. Pay attention as you do it and feel yourself calm.`
     }]
 }]
